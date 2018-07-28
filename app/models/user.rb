c980b0b7cb60413fc33dfdcbea0dea94683dc693
Alex Babenko <bob78alex@gmail.com>
2018-07-28 22:42:41 +0000

    Adding stripe payment forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_one :payment
+  accepts_nested_attributes_for :payment
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
