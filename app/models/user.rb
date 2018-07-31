206b27f421a9ed49240e5a41a6f7200e33fbeadc
Alex Babenko <bob78alex@gmail.com>
2018-07-31 00:25:32 +0000

    Adding image uploading features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :images
   has_one :payment
   accepts_nested_attributes_for :payment
   devise :database_authenticatable, :registerable, :confirmable,
